beabf376ee2a04722baefbd44789bcad3cbd89a6
herrick <huangz4@domain.com>
2018-10-17 10:25:30 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,124 +1,124 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Zhenyang Huang",
+      "designation": "Manager",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Bellevue",
+      "age": "26"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+     "name": "Ran Duan",
+      "designation": "Server",
+      "working_hour": "40",
+      "hourly_salary": "12",
+      "living_city": "Seattle",
+      "age": "24"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+     "name": "Oreo Li",
+      "designation": "Manager",
+      "working_hour": "40",
+      "hourly_salary": "14",
+      "living_city": "Seattle",
+      "age": "24"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+     "name": "Nhi Zhang",
+      "designation": "Server",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Lynwood",
+      "age": "26"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "name": "Ying Zhang",
+      "designation": "Marketing",
+      "working_hour": "30",
+      "hourly_salary": "15",
+      "living_city": "Bellevue",
+      "age": "28"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "name": "Han Du",
+      "designation": "Sales Manager",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Seattle",
+      "age": "32"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "name": "Qin Xie",
+      "designation": "Marketing Manager",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Seattle",
+      "age": "32"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "name": "Kendy Kho",
+      "designation": "Manager",
+      "working_hour": "40",
+      "hourly_salary": "16",
+      "living_city": "Shoreline",
+      "age": "38"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "name": "Jace Chen",
+      "designation": "Server",
+      "working_hour": "20",
+      "hourly_salary": "11",
+      "living_city": "Seattle",
+      "age": "24"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "name": "Nana Park",
+      "designation": "Server",
+      "working_hour": "30",
+      "hourly_salary": "12",
+      "living_city": "Seattle",
+      "age": "25"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "name": "Beryl Zhang",
+      "designation": "Server",
+      "working_hour": "30",
+      "hourly_salary": "12",
+      "living_city": "Seattle",
+      "age": "25"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "name": "Weijie Xie",
+      "designation": "Server",
+      "working_hour": "30",
+      "hourly_salary": "13",
+      "living_city": "Seattle",
+      "age": "28"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "name": "Ivan Han",
+      "designation": "Chef",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Seattle",
+      "age": "25"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "name": "Jiehui Li",
+      "designation": "Server",
+      "working_hour": "30",
+      "hourly_salary": "12",
+      "living_city": "Seattle",
+      "age": "25"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "name": "Fred Kho",
+      "designation": "Chef",
+      "working_hour": "40",
+      "hourly_salary": "15",
+      "living_city": "Seattle",
+      "age": "42"
     }
   ]
 }
